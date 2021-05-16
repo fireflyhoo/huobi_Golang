07067f1962a1ef8fa14c1169daf7d8b293823fa7
fireflyhoo <fireflyhoo@gmail.com>
2021-05-16 13:30:26 +0800

    --替换掉火币原有的包名

--- config/config.go
+++ config/config.go
@@ -2,6 +2,12 @@ package config
 
 var Host = "api.huobi.pro"
 var AccessKey = "xxxx"
+var SecretKey = "秘钥"
 var AccountId = "1234"
 var SubUid int64 = 5678
 var SubUids string = "5678"
+
+// 是否启用 http代理
+var HttpProxy = false
+var ProxyHost = "127.0.0.1"
+var ProxyPort = "18899"
--- internal/httprequest.go
+++ internal/httprequest.go
@@ -1,17 +1,25 @@
 package internal
 
 import (
+	"fmt"
+	"github.com/fireflyhoo/huobi_golang/config"
 	"github.com/fireflyhoo/huobi_golang/logging/perflogger"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 func HttpGet(url string) (string, error) {
 	logger := perflogger.GetInstance()
 	logger.Start()
+	proxy, err := url.Parse("http://" + config.ProxyHost + ":" + config.ProxyPort)
+	fmt.Print(proxy)
+	transport := &http.Transport{Proxy: http.ProxyURL(proxy)}
+	client := &http.Client{Transport: transport}
 
-	resp, err := http.Get(url)
+	rep, _ := http.NewRequest("GET", url, nil)
+	resp, err := client.Do(rep)
 	if err != nil {
 		return "", err
 	}
